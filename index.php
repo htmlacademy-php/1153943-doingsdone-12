1279e4c66e38ccdb1bbea64e4718526c0c2f4ccf
Vladislav <zhurkin111@bk.ru>
2021-02-18 22:02:01 +0300

    add errors

--- index.php
+++ index.php
@@ -3,50 +3,52 @@
 
     $show_complete_tasks = rand(0, 1);
 
-    $con = mysqli_connect("localhost", "root", "root", "schema");
-    mysqli_set_charset($con, "utf8");
-
     const USER_ID = 2;
 
-    $sqlList = "SELECT * FROM list WHERE user_id = ".USER_ID;
+    $errors = [];
 
-    if($_GET['category_id']) {
-        $sqlTasks = "SELECT * FROM tasks WHERE list_id =".$_GET['category_id']." AND user_id = ".USER_ID;
-    } else {
-        $sqlTasks = "SELECT * FROM tasks WHERE user_id = ".USER_ID;
+    function connect () {
+        $con = mysqli_connect("localhost", "root", "root", "schema");
+        mysqli_set_charset($con, "utf8");
+
+        if (!$con) {
+            $error = mysqli_error($con);
+            throw new Exception('Ошибка SQL запроса ' . $error);
+        }
+
+        return $con;
     }
 
-    $sqlTasksCount = "SELECT * FROM tasks WHERE user_id = ".USER_ID;
+    function getSqlTaskList($safeCategory, $user) {
+        $tasks = "SELECT * FROM tasks WHERE user_id = ".$user;
 
-    $sqlName = "SELECT name FROM users WHERE id = ".USER_ID;
+        if($safeCategory) {
+            $tasks = "SELECT * FROM tasks WHERE list_id =".$safeCategory." AND user_id = ".$user;
+        }
 
-    function getSqlArr($inquiry, $connect) {
+        return $tasks;
+    }
 
-        $result = mysqli_query($connect, $inquiry);
+    function getSqlArr($inquiry, $con) {
+
+        $result = mysqli_query($con, $inquiry);
 
         if (!$result) {
-            $error = mysqli_error($connect);
-            throw new Exception("Ошибка MySQL: " . $error);
+            $error = mysqli_error($con);
+            throw new Exception('Ошибка SQL запроса ' . $error);
         }
 
         return mysqli_fetch_all($result, MYSQLI_ASSOC);
     }
 
-    try {
-        $arrCategory = getSqlArr($sqlList, $con);
-        $arrCaseSheet = getSqlArr($sqlTasks, $con);
-        $arrCaseSheetCount = getSqlArr($sqlTasksCount, $con);
-        $arrNameUser = getSqlArr($sqlName, $con);
-    } catch (Exception $e) {
-        echo 'Выброшено исключение: ', $e->getMessage();
-    }
+    function nameUser($arr) {
+        $name = 'User';
 
-    foreach ($arrNameUser as $user) {
-        $nameUser = $user['name'];
-    }
+        foreach ($arr as $user) {
+            $name = $user['name'];
+        }
 
-    if ($con) {
-        mysqli_close($con);
+        return $name;
     }
 
     function getCountTasks($caseSheet, $category, $complete_tasks) {
@@ -60,7 +62,7 @@
         }
 
         return $count;
-    };
+    }
 
     function getTimeTask($date){
         $timeNow = time();
@@ -73,7 +75,7 @@
         }
 
         return false;
-    };
+    }
 
     function updateArray($caseSheet, $category, $countArr, $complete_tasks) {
 
@@ -101,13 +103,83 @@
         }
 
         return [$caseSheet, $category];
-    };
+    }
+
+    function project_existence_check($project_id){
+        $result = false;
+
+        $connect = connect();
+
+        if ($project_id) {
+
+            $sql = "SELECT * FROM `list` WHERE `id` = " . (string)$project_id;
+            $sql_result = mysqli_query($connect, $sql);
+
+            if ($sql_result && isset($sql_result->num_rows) && $sql_result->num_rows > 0) {
+                $result = true;
+            }
+        }
+
+        return $result;
+    }
+
+    function value_int_check($value_name){
+        $result = true;
+
+        if (!filter_input(INPUT_GET, $value_name, FILTER_SANITIZE_NUMBER_INT)) {
+            $result = false;
+        }
+
+        return $result;
+    }
+
+    function validate_project_id($project_id){
+        $result = false;
+
+        if (value_int_check('category_id') && project_existence_check($project_id)) {
+            $result = true;
+        }
+
+        return $result;
+    }
+
+    $connect = connect();
+    $safeCategory = mysqli_real_escape_string($connect, $_GET['category_id']);
+
+    $sqlTasks = getSqlTaskList($safeCategory, USER_ID);
+    $sqlList = "SELECT * FROM list WHERE user_id = ".USER_ID;
+    $sqlTasksCount = "SELECT * FROM tasks WHERE user_id = ".USER_ID;
+    $sqlName = "SELECT name FROM users WHERE id = ".USER_ID;
+
+    try {
+        $arrCategory = getSqlArr($sqlList, $connect);
+        $arrCaseSheet = getSqlArr($sqlTasks, $connect);
+        $arrCaseSheetCount = getSqlArr($sqlTasksCount, $connect);
+        $arrNameUser = getSqlArr($sqlName, $connect);
+    } catch (Exception $e) {
+        $errors[] = $e->getMessage();
+    }
+
+    $nameUser = nameUser($arrNameUser);
+
+    $current_project_id = $safeCategory ?? '';
+    $current_project_id = mysqli_real_escape_string($connect, (string)$current_project_id);
+
+    if ($connect) {
+        mysqli_close($connect);
+    }
 
     list($arrCaseSheet, $arrCategory) = updateArray($arrCaseSheet, $arrCategory, $arrCaseSheetCount, $show_complete_tasks);
     updateArray($arrCaseSheet, $arrCategory, $arrCaseSheetCount, $show_complete_tasks);
 
     $page_content = include_template('main.php', ['arrCategory' => $arrCategory, 'arrCaseSheet' => $arrCaseSheet, 'show_complete_tasks' => $show_complete_tasks]);
-    $layout_content = include_template('layout.php', ['content' => $page_content, 'title' => 'Дела в порядке', 'user' => $nameUser]);
+
+    if ($current_project_id) {
+        if (!validate_project_id($current_project_id)) {
+            $page_content = include_template('404.php', []);
+        }
+    }
+
+    $layout_content = include_template('layout.php', ['content' => $page_content, 'title' => 'Дела в порядке', 'user' => $nameUser, 'errors' => $errors]);
 
     print($layout_content);
-?>
